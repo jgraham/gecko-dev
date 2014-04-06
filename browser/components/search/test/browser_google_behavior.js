8a1ac83874ed77e9d2ddb8cd40a2524e1423f608
Matthew Noorenberghe <mozilla@noorenberghe.ca>
2014-04-06 17:50:06 -0400

    Bug 992270 - browser_bing/google_bahavior.js: Delay clicking the about:home search button from the mutation observer until that page has had a chance to respond itself. r=gavin

--- browser/components/search/test/browser_google_behavior.js
+++ browser/components/search/test/browser_google_behavior.js
@@ -120,7 +120,7 @@ function test() {
                 // Re-add the listener, and perform a search
                 gBrowser.addProgressListener(listener);
                 doc.getElementById("searchText").value = "foo";
-                doc.getElementById("searchSubmit").click();
+                setTimeout(() => doc.getElementById("searchSubmit").click(), 2000);
               }
             }
           });
